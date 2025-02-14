32c870c2c4edabe7b974dc00b93705a3f5ae94c9
georgetsai2028 <115137756+georgetsai2028@users.noreply.github.com>
2025-02-14 12:35:23 -0800

    added svg to greeting

--- src/greetings.jsx
+++ src/greetings.jsx
@@ -1,5 +1,16 @@
+
 function Greeting() {
-    return <h1>&quot; Ready to transfer into your Dream School? &quot;</h1>
+    return ( 
+    <>
+    <div>
+    <h1> Ready to transfer into your Dream School? </h1>
+    <button> Click Here to Learn More!</button>
+    </div>
+    <div>
+    <img src = {'/grad.svg'} alt = "gradution pic" />
+    </div>
+    </>
+)
 }
 
 export default Greeting;
--- src/main.jsx
+++ src/main.jsx
@@ -6,6 +6,6 @@ import Greeting from './greetings.jsx'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
+    <Greeting />
   </StrictMode>,
 )
