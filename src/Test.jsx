2e1cbb29e27f8989aff479bfe85f52eb1721f92c
georgetsai2028 <115137756+georgetsai2028@users.noreply.github.com>
2025-02-15 14:07:10 -0800

    testing rendering if list empty

--- src/Test.jsx
+++ src/Test.jsx
@@ -1,16 +1,27 @@
 export function ListItem(props){
     return <li> {props.animal}</li>
     
-}
+}   
 
 export function List(props){
-    return(
+    
+        if (!props.animals)
+        {
+            return <div> Loading...</div>
+        }
+
+        if (props.animals.length === 0)
+        {
+            return <div>There are no animals in the list!</div>
+        }
+        return (
         <ul>
             {props.animalList.map((animal)=> {
                 return animal.startsWith("T") && <ListItem key={animal} animal = {animal} />;
             })}
         </ul>
     );
+
 }
 
 export function Test(){
