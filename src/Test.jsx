7b1e10f5c7c199d5fb6e377c1a51949149f62b64
georgetsai2028 <115137756+georgetsai2028@users.noreply.github.com>
2025-02-14 18:28:18 -0800

    creating basic test template to change into react

---
+++ src/Test.jsx
@@ -0,0 +1,13 @@
+function Test() {
+    <>
+    <h1>Test title</h1>
+    <svg>
+        <circle cx="25" cy="75" r="20" stroke="green" stroke-width="2" />
+    </svg>
+    <form>
+        <input type="text"></input>
+    </form>
+    </>
+}
+
+export default Test;
--- src/main.jsx
+++ src/main.jsx
@@ -3,9 +3,10 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
 import Greeting from './greetings.jsx'
+import Test from './Test.jsx'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <Greeting />
+    <Test />
   </StrictMode>,
 )
