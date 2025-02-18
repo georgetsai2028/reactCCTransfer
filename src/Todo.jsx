afab559ded422d884265fe38eb982ff3dd57da29
georgetsai2028 <115137756+georgetsai2028@users.noreply.github.com>
2025-02-17 17:52:27 -0800

    added function as prop

--- src/Todo.jsx
+++ src/Todo.jsx
@@ -4,20 +4,26 @@ const todos = [
     { task: "Finish Math Homework", id: crypto.randomUUID }
 ]
 
-export function Button ({text = "Click Me", color = "purple", fontSize = "12"}) {
+export function Button ({text = "Click Me", color = "purple", fontSize = 12, handleClick }) {
     const buttonStyle ={
         color: color,
         fontSize: fontSize + 'px'
     };
-    return <button style = {buttonStyle}>{text}</button>
-}
+    return <button onClick={handleClick} style={buttonStyle}>
+        {text}
+    </button>
+};
+
 
 export function TodoList() {
+    const handleButtonClick = (url) => {
+        window.location.href = url;
+    };
     return (
         <>
         <Button text = "click me!" color = "blue" fontSize={28}/>
         <Button text = "no click me!" color = "red" fontSize={28}/>
-        <Button />
+        <Button handleClick={() => handleButtonClick ('www.theodinproject.com')} />
         <ul>
             {todos.map((todo) => (
             <li key={todo.id}>{todo.task}</li>
