efde7d684871fe92911fe8c2b0ca43dac4ded695
georgetsai2028 <115137756+georgetsai2028@users.noreply.github.com>
2025-02-17 16:01:43 -0800

    testing out prop destructing

--- src/Todo.jsx
+++ src/Todo.jsx
@@ -4,14 +4,24 @@ const todos = [
     { task: "Finish Math Homework", id: crypto.randomUUID }
 ]
 
-function TodoList() {
+export function Button ({text, color, fontSize}) {
+    const buttonStyle ={
+        color: color,
+        fontSize: fontSize + 'px'
+    };
+    return <button style = {buttonStyle}>{text}</button>
+}
+
+export function TodoList() {
     return (
+        <>
+        <Button text = "click me!" color = "blue" fontSize={28}/>
+        <Button text = "no click me!" color = "red" fontSize={28}/>
         <ul>
             {todos.map((todo) => (
             <li key={todo.id}>{todo.task}</li>
             ))}
         </ul>
+        </>
     )
 }
-
-export default TodoList;
--- src/main.jsx
+++ src/main.jsx
@@ -5,11 +5,10 @@ import App from './App.jsx'
 import Greeting from './greetings.jsx'
 import { Test, List, ListItem} from './Test.jsx'
 import DisplayProfile from './Profile.jsx'
-import TodoList from './Todo.jsx'
+import {TodoList, Button} from './Todo.jsx'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-  <Test />
   <TodoList />
   </StrictMode>,
 )
