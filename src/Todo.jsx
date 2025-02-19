c594c4cd9f3b8e141d3f91135d68467df7c5f87f
georgetsai2028 <115137756+georgetsai2028@users.noreply.github.com>
2025-02-19 00:13:13 -0800

    cleaning everything up for testing

--- src/Todo.jsx
+++ src/Todo.jsx
@@ -1,23 +1,7 @@
 import { useState } from 'react';
-import './Todo.css'
+import './Todo'
 
 const COLORS = ["red", "blue", "pink", "purple", "white"]
-const todos = [
-    { task: "Mow the backyard", id: crypto.randomUUID },
-    { task: "Work on Odin Project", id: crypto.randomUUID },
-    { task: "Finish Math Homework", id: crypto.randomUUID }
-]
-
-export function Button ({text = "Click Me", color = "purple", fontSize = 12, handleClick }) {
-
-     const buttonStyle = {
-        color: color,
-        fontSize: fontSize + 'px'
-    };
-    return <button onClick={handleClick} style={buttonStyle}>
-        {text}
-    </button>
-};
 
 export function ChangeColor(){
     const [backgroundColor, setBackgroundColor] = useState(COLORS[0]);
@@ -37,20 +21,4 @@ export function ChangeColor(){
     )
 }
 
-export function TodoList() {
-    const handleButtonClick = (url) => {
-        window.location.href = url;
-    };
-    return (
-        <>
-        <Button text = "click me!" color = "blue" fontSize={28}/>
-        <Button text = "no click me!" color = "red" fontSize={28}/>
-        <Button handleClick={() => handleButtonClick ('www.theodinproject.com')} />
-        <ul>
-            {todos.map((todo) => (
-            <li key={todo.id}>{todo.task}</li>
-            ))}
-        </ul>
-        </>
-    )
-}
+
--- src/main.jsx
+++ src/main.jsx
@@ -5,7 +5,7 @@ import App from './App.jsx'
 import Greeting from './greetings.jsx'
 import { Test, List, ListItem} from './Test.jsx'
 import DisplayProfile from './Profile.jsx'
-import {TodoList, Button, ChangeColor} from './Todo.jsx'
+import { ChangeColor} from './Todo.jsx'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
